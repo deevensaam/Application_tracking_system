b8d8ed435bb3cb86fd779e40872773075b9efe29
Sashi445 <sashidhar652@gmail.com>
2023-03-30 16:13:22 +0530

    Added models for the application

--- main/models.py
+++ main/models.py
@@ -1,15 +1,36 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
 class Job(models.Model):
-    pass
+    role = models.CharField(max_length=25)
+    description = models.CharField(max_length=300)
+    salary = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return f"id : {self.pk} role : {self.role} salary : {self.salary}"
 
 
-class Candidate(models.Model):
-    pass
+class Candidate(AbstractUser):
+    def __str__(self) -> str:
+        return f"Candidate - id : {self.pk} email : {self.email}"
 
 
+class Recruiter(AbstractUser):
+    def __str__(self) -> str:
+        return f"Recruiter - id : {self.pk} email : {self.email}" 
 
-class Recruiter(models.Model):
-    pass
 
+class JobApplication(models.Model):
+
+    class ApplicationStatus(models.TextChoices):
+        ACCEPTED = 'ACCEPTED', _('accepted')
+        REJECTED = 'REJECTED', _('rejected')
+ 
+    jobId = models.ForeignKey(to=Job.pk)
+    created_by = models.ForeignKey(to=Recruiter.pk)
+    applied_by = models.ForeignKey(to=Candidate.pk)
+    feedback_note = models.CharField(max_length=200)
+    user_note = models.CharField(max_length=200)
+    status = models.CharField(choices=ApplicationStatus.choices)
