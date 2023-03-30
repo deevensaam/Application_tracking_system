a8fabd16cab0afc6738238149aa8307db0c79fd8
deevensaam <deeven.pedapalli@gmail.com>
2023-03-30 15:18:45 +0530

    Initial commit

---
+++ main/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Job(models.Model):
+    pass
+
+
+class Candidate(models.Model):
+    pass
+
+
+
+class Recruiter(models.Model):
+    pass
+
